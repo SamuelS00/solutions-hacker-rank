a58126597f4f5cedbc934ae2858bf9925bb616eb
Samuel <samuelsoutosilva14@gmail.com>
2022-10-12 14:42:46 -0300

    feat: diagonal difference

---
+++ diagonal-difference/ex.js
@@ -0,0 +1,25 @@
+function diagonalDifference(arr) {
+  let leftDiagonal = 0;
+  let rightDiagonal = 0;
+  let reverseIndexCount = 0;
+  
+  arr.forEach((d2, d1Index) => {
+    if(!reverseIndexCount) 
+      reverseIndexCount = d2.length;
+
+    reverseIndexCount -= 1;
+
+    leftDiagonal += d2[d1Index];
+    rightDiagonal += d2[reverseIndexCount];
+  });
+
+  return Math.abs(leftDiagonal - rightDiagonal);
+}
+
+console.time();
+diagonalDifference(arr3x3);
+console.timeEnd();
+
+console.time();
+diagonalDifference2(arr3x3);
+console.timeEnd();
