b8c0b1cbbe5eb34c480bac8987396ed04f067b72
Samuel <samuelsoutosilva14@gmail.com>
2023-03-06 09:00:36 -0300

    feat: add the hurdle race exercise

---
+++ the-hurdle-race/ex.py
@@ -0,0 +1,14 @@
+def hurdleRace(k, height):
+    higher_number = max(height) if len(height) > 0 else 0
+    potions = higher_number - k
+
+    if potions <= 0:
+        return 0
+    else:
+        return potions
+
+
+if __name__ == "__main__":
+    print(hurdleRace(4, [1, 6, 3, 5, 2]))
+    print(hurdleRace(7, [2, 5, 4, 5, 2]))
+    print(hurdleRace(7, []))
